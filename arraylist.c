8187a620b977358ee8c31dd08c9be17eaa94f6d8
Replit user <>
2023-03-31 18:13:27 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -59,16 +59,11 @@ void* pop(ArrayList * l, int i){
     aux=l->data[(l->size)+1];
   }
   if(l->size==0) return NULL;
-
+  if(i<0) i=l->size+i;
   if(i>=0){
     for(int k = i ; k<l->size-1;k++){
       l->data[k]=l->data[k+1];
     }
-  }
-  if(i<0 && -i<l->size-1){ 
-    for(int k = (l->size+i) ; k<l->size-1;k++){
-      l->data[k]=l->data[k+1]; 
-    {
   }
   l->size--;
   return aux;

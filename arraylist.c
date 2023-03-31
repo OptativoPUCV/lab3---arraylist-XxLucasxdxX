6ec88a9559174c8876b64f306be209449ef8a7da
Replit user <>
2023-03-31 00:53:32 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -60,9 +60,9 @@ void* pop(ArrayList * l, int i){
 }
 
 void* get(ArrayList * l, int i){
-  if(i>l->size) return NULL;
+  if(i>l->size || i<0) return NULL;
   if(i>=0 && i< l->size) return l->data[i];
-    return NULL;
+  return NULL;
 }
 
 int get_size(ArrayList * l){
